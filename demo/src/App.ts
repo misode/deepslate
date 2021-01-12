fafd778e6964e6505c0bc78c6fd7a03f17be7e8d
Misode <Misoloo64@gmail.com>
2021-01-12 01:54:14 +0100

    Update demo

--- demo/src/App.ts
+++ demo/src/App.ts
@@ -2,6 +2,7 @@ import { NamedNbtTag, Structure } from "@webmc/core"
 import { StructureRenderer } from '@webmc/render';
 import { ResourceManager } from './ResourceManager'
 import nbt from 'nbt'
+import { mat4 } from 'gl-matrix'
 
 let viewDist = 4;
 let xRotation = 0.8;
@@ -27,35 +28,7 @@ async function main() {
 
   const renderer = new StructureRenderer(gl, resources, resources, resources.getBlockAtlas(), structure)
 
-  function render() {
-    yRotation = yRotation % (Math.PI * 2)
-    xRotation = Math.max(-Math.PI / 2, Math.min(Math.PI / 2, xRotation))
-    viewDist = Math.max(1, Math.min(20, viewDist))
-    renderer.drawStructure(xRotation, yRotation, viewDist);
-  }
-  requestAnimationFrame(render);
-
-  let dragPos: [number, number] | null = null
-  canvas.addEventListener('mousedown', evt => {
-    dragPos = [evt.clientX, evt.clientY]
-  })
-  canvas.addEventListener('mousemove', evt => {
-    if (dragPos) {
-      yRotation += (evt.clientX - dragPos[0]) / 100
-      xRotation += (evt.clientY - dragPos[1]) / 100
-      dragPos = [evt.clientX, evt.clientY]
-      requestAnimationFrame(render);
-    }
-  })
-  canvas.addEventListener('mouseup', () => {
-    dragPos = null
-  })
-  canvas.addEventListener('wheel', evt => {
-    viewDist += evt.deltaY / 100
-    requestAnimationFrame(render);
-  })
-
-  window.addEventListener('resize', () => {
+  function resize() {
     const displayWidth = canvas.clientWidth;
     const displayHeight = canvas.clientHeight;
 
@@ -64,6 +37,58 @@ async function main() {
       canvas.height = displayHeight;
 
       renderer.setViewport(0, 0, canvas.width, canvas.height)
+      return true
+    }
+  }
+
+  function render() {
+    resize()
+
+    yRotation = yRotation % (Math.PI * 2)
+    xRotation = Math.max(-Math.PI / 2, Math.min(Math.PI / 2, xRotation))
+    viewDist = Math.max(1, Math.min(20, viewDist))
+
+    const size = structure.getSize()
+    const viewMatrix = mat4.create();
+    mat4.translate(viewMatrix, viewMatrix, [0, 0, -viewDist]);
+    mat4.rotate(viewMatrix, viewMatrix, xRotation, [1, 0, 0]);
+    mat4.rotate(viewMatrix, viewMatrix, yRotation, [0, 1, 0]);
+    mat4.translate(viewMatrix, viewMatrix, [-size[0] / 2, -size[1] / 2, -size[2] / 2]);
+
+    renderer.drawGrid(viewMatrix);
+    renderer.drawStructure(viewMatrix);
+  }
+  requestAnimationFrame(render);
+
+  let dragPos: [number, number] | null = null
+  let dragButton: number
+  canvas.addEventListener('mousedown', evt => {
+    dragPos = [evt.clientX, evt.clientY]
+    dragButton = evt.button
+  })
+  canvas.addEventListener('mousemove', evt => {
+    if (dragPos) {
+      if (dragButton === 0) {
+        yRotation += (evt.clientX - dragPos[0]) / 100
+        xRotation += (evt.clientY - dragPos[1]) / 100
+      }
+      dragPos = [evt.clientX, evt.clientY]
+      requestAnimationFrame(render);
+    }
+  })
+  canvas.addEventListener('mouseup', evt => {
+    dragPos = null
+  })
+  canvas.addEventListener('contextmenu', evt => {
+    evt.preventDefault()
+  })
+  canvas.addEventListener('wheel', evt => {
+    viewDist += evt.deltaY / 100
+    requestAnimationFrame(render);
+  })
+
+  window.addEventListener('resize', () => {
+    if (resize()) {
       requestAnimationFrame(render);
     }
   })
