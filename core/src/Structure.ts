8674e4156c0ba8268fbfea00550e94471ff6df2a
Misode <Misoloo64@gmail.com>
2021-01-16 02:27:04 +0100

    Store nbt of blocks in structure

--- core/src/Structure.ts
+++ core/src/Structure.ts
@@ -3,33 +3,46 @@ import { NamedNbtTag, NbtTag, read, readListTag, readOptional, readTag } from ".
 
 export type BlockPos = [number, number, number]
 
+type CompoundNbt = { [key: string]: NbtTag }
+
 export class Structure {
   constructor(
     private size: BlockPos,
     private palette: BlockState[] = [],
-    private blocks: { pos: BlockPos, state: number }[] = []
+    private blocks: { pos: BlockPos, state: number, nbt?: CompoundNbt }[] = []
   ) {}
 
   public getSize() {
     return this.size
   }
 
-  public addBlock(pos: BlockPos, name: string, properties?: { [key: string]: string }) {
+  public addBlock(pos: BlockPos, name: string, properties?: { [key: string]: string }, nbt?: CompoundNbt) {
     const blockState = new BlockState(name, properties)
     let state = this.palette.findIndex(b => b.equals(blockState))
     if (state === -1) {
       state = this.palette.length
       this.palette.push(blockState)
     }
-    this.blocks.push({ pos, state })
+    this.blocks.push({ pos, state, nbt })
     return this
   }
 
   public getBlocks() {
     return this.blocks.map(b => ({
       pos: b.pos,
-      state: this.palette[b.state]
+      state: this.palette[b.state],
+      nbt: b.nbt
     }))
+  }
+
+  public getBlock(pos: BlockPos) {
+    const block = this.blocks.find(b => b.pos[0] === pos[0] && b.pos[1] === pos[1] && b.pos[2] === pos[2])
+    if (!block) return null
+    return {
+      pos: block.pos,
+      state: this.palette[block.state],
+      nbt: block.nbt
+    }
   }
 
   public static fromNbt(nbt: NamedNbtTag) {
@@ -47,7 +60,8 @@ export class Structure {
       .map(tags => {
         const pos = readListTag(tags, 'pos', 'int', 3) as BlockPos
         const state = readTag(tags, 'state', 'int') as number
-        return { pos, state }
+        const nbt = readOptional(() => readTag(tags, 'nbt', 'compound') as CompoundNbt, undefined)
+        return { pos, state, nbt }
       })
     return new Structure(size, palette, blocks)
   }
