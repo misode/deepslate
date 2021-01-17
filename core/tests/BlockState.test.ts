357ef6f55b973d00bcf463c6fac48d01c893b751
Misode <Misoloo64@gmail.com>
2021-01-17 22:44:04 +0100

    Add tests for core package

---
+++ core/tests/BlockState.test.ts
@@ -0,0 +1,44 @@
+import "mocha";
+import { expect } from 'chai';
+import { BlockState } from '../src/BlockState';
+
+describe('BlockState', () => {
+  it('getName', () => {
+    const state = new BlockState('minecraft:jigsaw', { orientation: 'east_up' })
+    const name = state.getName()
+    expect(name).to.be.a('string')
+    expect(name).to.equal('minecraft:jigsaw')
+  })
+
+  it('getProperties', () => {
+    const state = new BlockState('minecraft:piston', { extended: 'false', facing: 'up' })
+    const props = state.getProperties()
+    expect(props).to.be.an('object').with.keys('extended', 'facing')
+    expect(props['extended']).to.equal('false')
+    expect(props['facing']).to.equal('up')
+  })
+
+  it('getProperty', () => {
+    const state = new BlockState('minecraft:piston', { extended: 'false', facing: 'up' })
+    expect(state.getProperty('extended')).to.equal('false')
+    expect(state.getProperty('facing')).to.equal('up')
+  })
+
+  it('equals', () => {
+    const stateA = new BlockState('minecraft:piston', { extended: 'false', facing: 'up' })
+    const stateB = new BlockState('minecraft:piston', { extended: 'false', facing: 'up' })
+    expect(stateA.equals(stateB)).to.be.true
+    const stateC = new BlockState('minecraft:sticky_piston', { extended: 'false', facing: 'up' })
+    expect(!stateA.equals(stateC)).to.be.true
+    const stateD = new BlockState('minecraft:piston', { facing: 'up' })
+    expect(!stateA.equals(stateD)).to.be.true
+    const stateE = new BlockState('minecraft:piston', { extended: 'false', facing: 'down' })
+    expect(!stateA.equals(stateE)).to.be.true
+  })
+
+  it('toString', () => {
+    const state = new BlockState('minecraft:piston', { extended: 'false', facing: 'up' })
+    const string = state.toString()
+    expect(string).to.equal('minecraft:piston[extended=false,facing=up]')
+  })
+})
