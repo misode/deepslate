287e98b5b25faf1ad0a88b03a613e02c2df00a0a
Misode <Misoloo64@gmail.com>
2021-08-23 21:12:46 +0200

    Changes to building and publishing

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,29 @@
+import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 import { defineConfig } from 'rollup'
+import dts from 'rollup-plugin-dts'
 import { terser } from 'rollup-plugin-terser'
+import pkg from './package.json'
 
-export default defineConfig({
-	input: 'src/index.ts',
-	output: [
-		// { dir: 'lib', format: 'cjs' },
-		{ file: 'dist/index.min.js', format: 'iife', name: 'deepslate', plugins: [terser()] },
-		{ file: 'dist/index.esm.js', format: 'esm' },
-	],
-	plugins: [
-		resolve(),
-		typescript({ outputToFilesystem: true }),
-	],
-})
+export default defineConfig([
+	{
+		input: 'src/index.ts',
+		output: { file: pkg.unpkg, format: 'umd', sourcemap: true, name: 'deepslate' },
+		plugins: [commonjs(), resolve(), typescript(), terser()],
+	},
+	{
+		external: ['pako', 'gl-matrix'],
+		input: 'src/index.ts',
+		output: [
+			{ file: pkg.main, format: 'cjs', sourcemap: true },
+			{ file: pkg.module, format: 'es', sourcemap: true },
+		],
+		plugins: [typescript(), terser()],
+	},
+	{
+		input: 'src/index.ts',
+		output: [{ file: pkg.types, format: 'es' }],
+		plugins: [dts({ compilerOptions: { composite: false, incremental: false } })],
+	},
+])
