8be2e43dfc4b90c7f6ec6abdde8d947e708733e2
Misode <Misoloo64@gmail.com>
2021-08-30 05:19:56 +0200

    Add spline and multinoise demo

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
 			input: {
 				main: resolve(__dirname, 'index.html'),
 				render: resolve(__dirname, 'render/index.html'),
+				splines: resolve(__dirname, 'splines/index.html'),
+				multinoise: resolve(__dirname, 'multinoise/index.html'),
 			},
 		},
 	},
