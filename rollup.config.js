c50a842270f275b9fe01492f6af6ac413a99b63a
Misode <Misoloo64@gmail.com>
2022-02-21 20:14:09 +0100

    Use esbuild

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,24 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
-import typescript from '@rollup/plugin-typescript'
 import { defineConfig } from 'rollup'
 import dts from 'rollup-plugin-dts'
+import esbuild from 'rollup-plugin-esbuild'
 import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
+const dev = process.env.ROLLUP_WATCH === 'true'
+
 export default defineConfig([
-	{
+	...dev ? [] : [{
 		input: 'src/index.ts',
 		output: { file: pkg.unpkg, format: 'umd', sourcemap: true, name: 'deepslate' },
-		plugins: [commonjs(), resolve(), typescript(), terser()],
-	},
+		plugins: [
+			commonjs(),
+			resolve(),
+			esbuild(),
+			!dev && terser(),
+		],
+	}],
 	{
 		external: ['pako', 'gl-matrix', 'md5'],
 		input: 'src/index.ts',
@@ -19,11 +26,14 @@ export default defineConfig([
 			{ file: pkg.main, format: 'cjs', sourcemap: true },
 			{ file: pkg.module, format: 'es', sourcemap: true },
 		],
-		plugins: [typescript(), terser()],
+		plugins: [
+			esbuild(),
+			!dev && terser(),
+		],
 	},
-	{
+	...dev ? [] : [{
 		input: 'src/index.ts',
 		output: [{ file: pkg.types, format: 'es' }],
 		plugins: [dts({ compilerOptions: { composite: false, incremental: false } })],
-	},
+	}],
 ])
